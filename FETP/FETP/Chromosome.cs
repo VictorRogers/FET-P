c7954b4ac2d7c5494b34ade6b91b37c18a65207c
Vic <victor.r.rogers@gmail.com>
2016-02-28 16:46:49 -0600

    Setup classes for the GA Controller and chromosomes

---
+++ FETP/FETP/Chromosome.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FETP
+{
+
+    /*************************************************************************\
+    Class: Chromosome
+    Description: Each chromosome is a potential calendar solution. This class
+                 contains all of the relevant information for each solution.
+    \**************************************************************************/ 
+    class Chromosome
+    {
+    }
+}
---
+++ FETP/FETP/Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FETP
+{
+    /*************************************************************************\
+    Class: Controller
+    Description: Includes all of the primary functions needed for the genetic
+                 algorithm and evolving the chromosomes to find a solution.
+    \**************************************************************************/ 
+    class Controller
+    {
+    }
+}
