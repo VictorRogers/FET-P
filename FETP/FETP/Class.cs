64d4afd4893a50c535f51be49e667ac9d93c82cd
Vic <victor.r.rogers@gmail.com>
2016-03-28 13:24:37 -0500

    Formatting and cleanup to the class "Class"

--- FETP/FETP/Class.cs
+++ FETP/FETP/Class.cs
@@ -6,7 +6,6 @@ using System.Threading.Tasks;
 
 namespace FETP
 {
-
     /**************************************************************************\
     Class: Class
     Description: Contains basic information for a single class and functions 
@@ -14,45 +13,77 @@ namespace FETP
     \**************************************************************************/ 
     public class Class
     {
-
         /**************************************************************************\
-        Class - Data Members 
+        Class: Class
+        Section: Data Members 
         \**************************************************************************/
         protected TimeSpan startTime;
         protected TimeSpan endTime;
         protected int enrollment;
         protected List<DayOfWeek> daysMeet;
 
+
         /**************************************************************************\
-        Class - Properties 
+        Class: Class
+        Section: Properties
+        TODO: Let me know if headers for properties are too excessive (VR)
+        \**************************************************************************/
+        /**************************************************************************\
+        Property: StartTime
+        Description:
+        TODO: Add a description
         \**************************************************************************/
         public TimeSpan StartTime
         {
             get { return this.startTime; }
             set { this.startTime = value; }
         }
+
+
+        /**************************************************************************\
+        Property: EndTime 
+        Description:
+        TODO: Add a description
+        \**************************************************************************/
         public TimeSpan EndTime
         {
             get { return this.endTime; }
             set { this.startTime = value; }
         }
+
+
+        /**************************************************************************\
+        Property: Enrollment 
+        Description:
+        TODO: Add a description
+        \**************************************************************************/
         public int Enrollment
         {
             get { return this.enrollment; }
             set { this.enrollment = value; }
         }
+
+
+        /**************************************************************************\
+        Property: DaysMeet 
+        Description:
+        TODO: Add a description
+        \**************************************************************************/
         public List<DayOfWeek> DaysMeet
         {
             get { return this.daysMeet; }
             set { this.daysMeet = value; }
         }
 
+
         /**************************************************************************\
-        Class - Methods 
+        Class: Class
+        Section: Methods 
         \**************************************************************************/
         /**************************************************************************\
         Constructor: Default 
         Description: Takes in data values and creates class with those values
+        TODO: Change name to Overloaded constructor in header?
         \**************************************************************************/
         public Class(TimeSpan inStartTime, TimeSpan inEndTime, int inEnrollment, List<DayOfWeek> inDaysMeet)
         {
@@ -70,6 +101,7 @@ namespace FETP
                 this.daysMeet = inDaysMeet;
             }
         }
+
 
         /**************************************************************************\
         Method: Display
@@ -89,18 +121,20 @@ namespace FETP
             Console.WriteLine("");
         }
 
-        // TODO: this function does not do anything. The complications of writing a hash function is not needed for current program
+
         /**************************************************************************\
         Method: GetHashCode
         Description: Overloaded Hash function. It is improperly implemented 
                      due to the complexity being too high and the function
                      will not be used. C# requires it to be overloaded if
                      comparison is overloaded.
+        // TODO: this function does not do anything. The complications of writing a hash function are not needed for current program
         \**************************************************************************/
         public override int GetHashCode()
         {
             return base.GetHashCode();
         }
+
 
         /**************************************************************************\
         Method: Equals
@@ -119,26 +153,16 @@ namespace FETP
             else return false;
         }
 
-        /**************************************************************************\
-        Method: Equals
-        Description: Overloaded Equals function. It is *possibly* improperly  
-                     implemented due to the complexity being too high and the 
-                     function will not be used. C# requires it to be overloaded 
-                     if comparison is overloaded.
-                     This one is the overloaded one to operate with Class.
-                     likely wrong
-        \**************************************************************************/
-        /*
-        public bool Equals(Class inClass)
-        {
-            return (this.StartTime == inClass.StartTime && this.EndTime == inClass.EndTime && this.Enrollment == inClass.Enrollment && this.DaysMeet == inClass.DaysMeet);
-        }
-        */
 
         /**************************************************************************\
-        Method:  
+        Class: Class
+        Section: Overloaded Operators 
+        \**************************************************************************/
+        /**************************************************************************\
+        Operator: == 
         Description: Should take a chromosome as an input and output its fitness
                      score.
+        TODO: Remove ambiguity between overloaded == and Equals()
         \**************************************************************************/
         public static bool operator ==(Class class1, Class class2)
         {
@@ -146,8 +170,9 @@ namespace FETP
             return (class1.StartTime == class2.StartTime && class1.EndTime == class2.EndTime && class1.Enrollment == class2.Enrollment && class1.DaysMeet == class2.DaysMeet); // TODO: comparing list should work
         }
 
+
         /**************************************************************************\
-        Method:  
+        Operator: !=
         Description: Should take a chromosome as an input and output its fitness
                      score.
         \**************************************************************************/
@@ -156,6 +181,5 @@ namespace FETP
            // if (class1 == null && class2 == null) return false;
             return (class1.StartTime != class2.StartTime || class1.EndTime != class2.EndTime || class1.Enrollment != class2.Enrollment || class1.DaysMeet != class2.DaysMeet); // yay. used cs 245 to make this code faster
         }
-
-    } // end class Class
+    }
 }
