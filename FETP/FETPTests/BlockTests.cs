630897ff2a80aa3d78e7ddb8bb8489aff6a0d727
Vic <victor.r.rogers@gmail.com>
2016-04-23 18:02:42 -0500

    Added unit tests

---
+++ FETP/FETPTests/BlockTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FETP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FETP.Tests
+{
+    [TestClass()]
+    public class BlockTests
+    {
+        [TestMethod()]
+        public void BlockTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void addClassTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void isClassInGroupTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void doesClassOverlapWithBlockTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DisplayAllClassesTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ FETP/FETPTests/FETP_ControllerTests.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FETP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FETP.Tests
+{
+    [TestClass()]
+    public class FETP_ControllerTests
+    {
+        [TestMethod()]
+        public void ComputeVarianceOfBlocksTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void doClassesOverlapTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void doClassTimesOverlapTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void getNumberOfDaysInCommonTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void getNumberOfOverlappingClassesTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void doAnyClassesNotOverlapTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GroupClassTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GroupClassesTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CoalesceClassesTogetherTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void removeClassTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
+}
