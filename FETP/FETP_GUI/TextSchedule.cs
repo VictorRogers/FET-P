fb91298cfd16c6cef966ad1b89fe9f2664805327
abrown6 <abrown6@una.edu>
2016-04-26 16:12:05 -0500

    Text schedule now presents in format given in SRS.
    There's a bug with switching an exam group into an empty spot.

--- FETP/FETP_GUI/TextSchedule.cs
+++ FETP/FETP_GUI/TextSchedule.cs
@@ -50,24 +50,33 @@ namespace FETP_GUI
         /// </summary>
         public void DisplayBlocks()
         {
+            int dayNumber = 0;
+
             richTextBox1.Text += "\n***************************************";
-            richTextBox1.Text += "\nSCHEDULED BLOCKS INFORMATION";
+            richTextBox1.Text += "\nSCHEDULED CLASSES";
             richTextBox1.Text += "\n***************************************";
+            richTextBox1.Text += "\nClasses which meet\t\t\tExam Time";
             for (int i = 0; i < _schedule.Blocks.Length; i++)
             {
                 if (_schedule.Blocks[i] != null)
                 {
 
-                    richTextBox1.Text += ("\n**************");
-                    richTextBox1.Text += ("\nBlock");
-                    richTextBox1.Text += ("\n**************");
-                    richTextBox1.Text += string.Format("\nBlock start time: {0}", _schedule.StartTimesOfExams[i % _schedule.NumberOfTimeSlotsAvailablePerDay]);
-                    DisplayBlock(_schedule.Blocks[i]);
-                    richTextBox1.Text += ("\nClasses In Block");
-                    richTextBox1.Text += ("\n********");
+                    //richTextBox1.Text += ("\n**************");
+                    //richTextBox1.Text += ("\nGroup #" + (i + 1).ToString());
+                    //richTextBox1.Text += ("\n**************");
+                    //richTextBox1.Text += string.Format("\nBlock start time: {0}", _schedule.StartTimesOfExams[i % _schedule.NumberOfTimeSlotsAvailablePerDay]);
+                    //DisplayBlock(_schedule.Blocks[i]);
+                    //richTextBox1.Text += ("\nClasses In Block");
+                    //richTextBox1.Text += ("\n********");
 
-                    DisplayAllClasses(_schedule.Blocks[i].ClassesInBlock);
+                    if (_schedule.StartTimesOfExams[i%_schedule.NumberOfTimeSlotsAvailablePerDay].Equals(_schedule.ExamsStartTime))
+                    {
+                        dayNumber++;
+                        richTextBox1.Text += "\n\n";
+                    }
+                    DisplayAllClasses(_schedule.Blocks[i].ClassesInBlock, _schedule.StartTimesOfExams[i % _schedule.NumberOfTimeSlotsAvailablePerDay], _schedule.ExamsLength, dayNumber);
                     richTextBox1.Text += "\n";
+                    //richTextBox1.Text += "\n";
 
                 }
                 else
@@ -78,18 +87,18 @@ namespace FETP_GUI
             }
 
             richTextBox1.Text += ("\n*******************************************");
-            richTextBox1.Text += ("\nNON SCHEDULED BLOCKS INFORMATION");
+            richTextBox1.Text += ("\nNON SCHEDULED CLASSES");
             richTextBox1.Text += ("\n*******************************************");
             foreach (Block block in _schedule.LeftoverBlocks)
             {
-                richTextBox1.Text += ("\n**************");
-                richTextBox1.Text += ("\nBlock");
-                richTextBox1.Text += ("\n**************");
-                DisplayBlock(block);
-                richTextBox1.Text += ("\nClasses In Block");
-                richTextBox1.Text += ("\n********");
+                //richTextBox1.Text += ("\n**************");
+                //richTextBox1.Text += ("\nBlock");
+                //richTextBox1.Text += ("\n**************");
+                //DisplayBlock(block);
+                //richTextBox1.Text += ("\nClasses In Block");
+                //richTextBox1.Text += ("\n********");
                 DisplayAllClasses(block.ClassesInBlock);
-                richTextBox1.Text += ("\n");
+                //richTextBox1.Text += ("\n");
             }
         }
 
@@ -110,24 +119,86 @@ namespace FETP_GUI
         /// <summary>
         /// Add a description
         /// </summary>
+        public void DisplayAllClasses(List<Class> classesInBlock, TimeSpan startTime, TimeSpan examLength, int dayNumber)
+        {
+            foreach (Class cl in classesInBlock)
+                DisplayScheduledClass(cl, startTime, examLength, dayNumber);
+        }
+
+        /// <summary>
+        /// Add a description
+        /// </summary>
         public void DisplayAllClasses(List<Class> classesInBlock)
         {
             foreach (Class cl in classesInBlock)
-                DisplayClass(cl);
+                DisplayUnscheduledClass(cl);
         }
 
         /// <summary>
         /// Displays all information stored in a Class instance with formatting.
         /// </summary>
-        public void DisplayClass(Class cl)
+        public void DisplayScheduledClass(Class cl, TimeSpan startTime, TimeSpan examLength, int dayNumber)
         {
-            richTextBox1.Text += "\nDays Meet: ";
+            richTextBox1.Text += "\n";
             foreach (DayOfWeek day in cl.DaysMeet)
-                richTextBox1.Text += string.Format("{0} ", day);
+            {
+                switch (day)
+                {
+                    case DayOfWeek.Monday:
+                        richTextBox1.Text += "M";
+                        break;
+                    case DayOfWeek.Tuesday:
+                        richTextBox1.Text += "T";
+                        break;
+                    case DayOfWeek.Wednesday:
+                        richTextBox1.Text += "W";
+                        break;
+                    case DayOfWeek.Thursday:
+                        richTextBox1.Text += "R";
+                        break;
+                    case DayOfWeek.Friday:
+                        richTextBox1.Text += "F";
+                        break;
+                }
+            }
 
-            richTextBox1.Text += string.Format("\nStart Time: {0}", cl.StartTime);
-            richTextBox1.Text += string.Format("\nEnd Time: {0}", cl.EndTime);
-            richTextBox1.Text += string.Format("\nEnrollment: {0}", cl.Enrollment);
+            richTextBox1.Text += string.Format("\t{0}", cl.StartTime);
+            richTextBox1.Text += string.Format(" - {0}", cl.EndTime);
+            //richTextBox1.Text += string.Format("\nEnrollment: {0}", cl.Enrollment);
+            richTextBox1.Text += string.Format("\t\t{0}", startTime);
+            richTextBox1.Text += string.Format(" - {0}", startTime+examLength);
+            richTextBox1.Text += string.Format("\t\tDay {0}", dayNumber);
+            richTextBox1.Text += "\n";
+        }
+
+        public void DisplayUnscheduledClass(Class cl)
+        {
+            richTextBox1.Text += "\n";
+            foreach (DayOfWeek day in cl.DaysMeet)
+            {
+                switch (day)
+                {
+                    case DayOfWeek.Monday:
+                        richTextBox1.Text += "M";
+                        break;
+                    case DayOfWeek.Tuesday:
+                        richTextBox1.Text += "T";
+                        break;
+                    case DayOfWeek.Wednesday:
+                        richTextBox1.Text += "W";
+                        break;
+                    case DayOfWeek.Thursday:
+                        richTextBox1.Text += "R";
+                        break;
+                    case DayOfWeek.Friday:
+                        richTextBox1.Text += "F";
+                        break;
+                }
+            }
+
+            richTextBox1.Text += string.Format("\t{0}", cl.StartTime);
+            richTextBox1.Text += string.Format(" - {0}", cl.EndTime);
+            //richTextBox1.Text += string.Format("\nEnrollment: {0}", cl.Enrollment);
             richTextBox1.Text += "\n";
         }
     }
